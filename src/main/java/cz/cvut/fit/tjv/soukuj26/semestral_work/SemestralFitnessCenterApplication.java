8398e8520082c66012bbcce7d2b4bdfe066fdbcc
Jan Soukup <souki@hotmail.cz>
2021-12-26 16:19:45 +0100

    Added CrossOrigin to enable client.

--- src/main/java/cz/cvut/fit/tjv/soukuj26/semestral_work/SemestralFitnessCenterApplication.java
+++ src/main/java/cz/cvut/fit/tjv/soukuj26/semestral_work/SemestralFitnessCenterApplication.java
@@ -2,6 +2,10 @@ package cz.cvut.fit.tjv.soukuj26.semestral_work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SemestralFitnessCenterApplication {
@@ -10,4 +14,26 @@ public class SemestralFitnessCenterApplication {
 		SpringApplication.run(SemestralFitnessCenterApplication.class, args);
 	}
 
+	/**
+	 * Allows all api requests to go through, when something fails, throws exception that can be read by human
+	 * Probably filters which apis get access to server?
+	 * <a href = "https://stackoverflow.com/questions/53978113/spring-boot-with-angular-access-to-xmlhttprequest-at-http-localhost8080-f">StackOverflow</a>
+	 * @return Cors
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		//This throws errors for "*" while we allowCredentials
+		//config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
